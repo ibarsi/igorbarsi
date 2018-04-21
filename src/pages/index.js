11a2bda21b7dff7e92baf5502bab4e13ae0cd1b5
Igor Barsi <ibarsi92@gmail.com>
2018-04-21 12:59:08 -0400

    Newsletter/signup form (#25)
    * Added bare-bones signup form component, no styles.
    
    * Styled signup form and added landing page copy to support it.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
+import SignupForm from '../components/SignupForm';
 import { buildPageTitle } from '../utils';
 
 import './index.css';
@@ -38,6 +39,24 @@ const IndexPage = () => {
             <p className='paragraph paragraph--intro'>
                 {'Much of my writing consists of opinion posts and think pieces targeted at engineers, however the core concepts are far reaching and applicable to many disciplines. I don’t have all the answers, but I’m happy to share what I have learned in hopes that my thoughts and experiences might impact readers in a positive way.'}
             </p>
+
+            <br />
+
+            <h2 className='title title--sub'>
+                Join Us Now
+            </h2>
+
+            <p className='paragraph paragraph--intro'>
+                {'I feel very lucky to have the priviledge to share my knowledge and opinions with such a broad audience of makers, managers and the like. Together, I believe we can continually broaden our reach and impact millions by delivering the highest quality content that\'s guranteed to leave a lasting impact and bring value to those that consume it.'}
+            </p>
+
+            <p className='paragraph paragraph--intro'>
+                Ready to join us? Great! Just enter your email and sign up for my free newsletter below.
+            </p>
+
+            <div>
+                <SignupForm />
+            </div>
         </div>
 
         <ul className='clearfix social__list'>
