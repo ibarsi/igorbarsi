9e93a5097d0e0511a06118b2cf17bbd69de1ffd1
Igor Barsi <ibarsi92@gmail.com>
2018-04-23 18:57:53 -0400

    Added description and title to newsletter page.

--- src/pages/newsletter/index.js
+++ src/pages/newsletter/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 import SignupForm from '../../components/SignupForm';
+import { buildPageTitle } from '../../utils';
 
 const Newsletter = () => {
+    const title = buildPageTitle('Free Newsletter');
+    const description = 'Sign up for this free newsletter that will teach you the skills you need to become an effective software engineer and leader through shared lessons and concepts.';
+
     return <div className='content-container'>
+        <Helmet>
+            <title>
+                { title }
+            </title>
+
+            <meta name='description' content={ description } />
+            <meta property='og:title' content={ title } />
+        </Helmet>
         <h1 className='title'>
           Join my Weekly Newsletter
         </h1>
