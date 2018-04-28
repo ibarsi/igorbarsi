e9555cc121146bc5a0fe5fba7ed33ca0612c6d31
Igor Barsi <ibarsi92@gmail.com>
2018-04-28 10:59:58 -0400

    ♻️  Extracted social link icons into separate components for re-use.

---
+++ src/components/Icons/index.js
@@ -0,0 +1,15 @@
+import TwitterIcon from './twitter';
+import LinkedInIcon from './linkedIn';
+import MediumIcon from './medium';
+import GitHubIcon from './github';
+import StackOverflowIcon from './stackOverflow';
+
+import './icons.css';
+
+export default {
+    TwitterIcon,
+    LinkedInIcon,
+    MediumIcon,
+    GitHubIcon,
+    StackOverflowIcon,
+};
