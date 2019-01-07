60b1d93dc20c7e3a2ccd4a2d99dec4ed3a98ffd7
Igor Barsi <ibarsi92@gmail.com>
2019-01-07 14:55:00 -0500

    ✨ Add custom 404 page.

---
+++ src/pages/404.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const NotFoundPage = () => {
+  const title = 'Not Found - 404';
+  const description =
+    'Oops! Page not found :( Please return to www.igorbarsi.com.';
+
+  return (
+    <div>
+      <Helmet>
+        <title>{title}</title>
+
+        <meta name="description" content={description} />
+        <meta property="og:title" content={title} />
+      </Helmet>
+
+      <div className="content-container">
+        <header>
+          <h1 className="title">Oops! Page not found 😞</h1>
+        </header>
+
+        <p className="paragraph paragraph--intro">
+          Looks like we weren't able to find the page you were looking for.
+          Sorry about that!
+        </p>
+      </div>
+    </div>
+  );
+};
+export default NotFoundPage;
