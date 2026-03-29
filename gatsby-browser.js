2652e2bf4114519d1b5465c71f17b8bbec9b9941
Igor Barsi <ibarsi92@gmail.com>
2026-03-29 14:02:23 -0400

    ✨ ibarsi/astro (#88)
    * ✨ astro
    
    Migrate away from Gatsby to Astro
    
    * 💚 netlify
    
    Add toml file with basic config

--- gatsby-browser.js
+++
@@ -1,3 +0,0 @@
-import './src/layouts/normalize.css';
-import './src/layouts/main.css';
-import './src/layouts/fonts.css';
--- src/components/Icons/index.js
+++
@@ -1,9 +0,0 @@
-import { TwitterIcon } from './twitter';
-import { LinkedInIcon } from './linkedIn';
-import { MediumIcon } from './medium';
-import { GitHubIcon } from './github';
-import { StackOverflowIcon } from './stackOverflow';
-
-import './icons.css';
-
-export { TwitterIcon, LinkedInIcon, MediumIcon, GitHubIcon, StackOverflowIcon };
